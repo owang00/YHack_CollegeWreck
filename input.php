6ea61a7500552111b95d73431a0988d2f2fa00fa
owang00 <47338294+owang00@users.noreply.github.com>
2019-10-26 14:55:49 -0400

    Add files via upload

--- input.php
+++ input.php
@@ -1,20 +1,9 @@
-<?php
-	# ini_set('display_errors', 1);
+<html>
+<body>
 
-	if(isset($_POST['SubmitButton'])) { //check if form was submitted
-		$GPA = $_POST['GPA']; //get input text
-		$message = "Success! Your GPA is : ".$GPA; 
-		$testType = $_POST['testType'];
-		$message1 = "Success! The type of testType : ".$testType;
-		$score = $_POST['score'];
-		$message2 = "Success! Your score is : ".$score;
-		
-		header("Location:https://owang00.github.io/results.html");
-		/*
-		if(true) {
-			$testMessage = "Reached this step. Executing python file for FFEMPG.";
-			
-			$output = shell_exec('python3 downloader.py '.escapeshellarg($ESN).' '.escapeshellarg($numberOfFrame).' '.escapeshellarg($start).' '.escapeshellarg($end).' '.escapeshellarg($timelapseType).' '.escapeshellarg($frameType) );
-		}*/
-	}
-?>
+	WTF <?php echo $_POST["GPA"]; ?><br>
+	<?php echo $_POST["testType"]; ?><br>
+	<?php echo $_POST["score"]; ?><br>
+
+</body>
+</html>
