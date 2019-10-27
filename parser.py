7dfa4c56138d6cff0b94623f684ee2a5d1d3a147
Austin Tian <47795058+austintian03@users.noreply.github.com>
2019-10-27 01:35:25 -0400

    cmd line arguments for parser
    Writes to html file a formatted output of schools for user input

--- parser.py
+++ parser.py
@@ -1,9 +1,9 @@
 #This is my first Python program since high school.hi
 import os
-
+import sys
 gpa = sys.argv[1]
-scoreType = sys.argv[3]
-score = int(sys.argv[4])
+scoreType = sys.argv[2]
+score = int(sys.argv[3])
 
 collegeFile = open('colleges.txt')
 collegeContent = collegeFile.readlines()
@@ -21,11 +21,18 @@ writeFile.write('''<!DOCTYPE html>
    <head>
       <title>College Wrecks</title>
       <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
    </head>
    <body>
-      <p>''')     #Write your table stuff after <p>
+      <div class="row form-group">
+         <div class="col-md-4">
+            <p>''')
 
-writeFile.write('<br>Safety:<br>')
+writeFile.write('''<strong>Safety</strong><br> ''')
 for element in cc:
     if(scoreType.lower() == 'sat'):
         string = cc[element][4]
@@ -37,9 +44,14 @@ for element in cc:
         lo = int(string[:dash])
         mid = (lo+up)/2
         if(score >= up + (up - lo)/4):
-            writeFile.write('*'+cc[element][0]+'<br>')
+            writeFile.write('- '+cc[element][0]+'<br> ')
 
-writeFile.write('<br><strong>Target:</strong><br>')
+writeFile.write('''
+            </p>
+         </div>       
+         <div class="col md-4">
+            <p><strong>Target:</strong><br> ''')
+
 for element in cc:
     if(scoreType.lower() == 'sat'):
         string = cc[element][4]
@@ -51,9 +63,13 @@ for element in cc:
         lo = int(string[:dash])
         mid = (lo+up)/2
         if(score >= mid + (up - lo)/4 and score < up + (up - lo)/4):
-            writeFile.write('*'+cc[element][0]+'<br>')
+            writeFile.write('- '+cc[element][0]+'<br> ')
     
-writeFile.write('<br><strong>Reaches:</strong><br>')
+writeFile.write('''
+            </p>
+         </div>
+         <div class="col md-4">
+            <p><strong>Reaches:</strong><br> ''')
 for element in cc:
     if(scoreType.lower() == 'sat'):
         string = cc[element][4]
@@ -63,11 +79,14 @@ for element in cc:
         dash = string.index('â€“')
         up = int(string[dash+3:])
         lo = int(string[:dash])
-        mid = (lo+up)/2
+        mid = (lo+up)//2
         if(score < mid + (up - lo)/4):
-            writeFile.write('*'+cc[element][0]+'<br>')
+            writeFile.write('- '+cc[element][0]+'<br> ')
 
-writeFile.write('''      </p>
+writeFile.write('''
+            </p>
+         </div>
+      </div>
    </body>
 </html>''')
 
